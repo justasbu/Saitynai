79397f99a0091770c34f943602657a9064bb0fa5
justas-GL62M-7RDX <justas.bucionis@gmail.com>
2020-09-23 01:08:30 +0300

    Added static folder

---
+++ client/src/PostService.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+
+const url = 'api/posts/';
+
+class PostService {
+    // Get Posts
+ 
+    static getPosts() {
+        return new Promise ((resolve,reject) => {
+            axios.get(url).then((res) => {
+                const data = res.data;
+                resolve(
+                    data.map(post => ({
+                        ...post,
+                        createdAt: new Date(post.createdAt)
+                    }))
+                );
+            })
+            .catch((err)=> {
+                reject(err);
+            })
+            
+        });
+    }
+
+    // Create Posts
+    static insertPost(text) {
+        return axios.post(url, {
+            text
+        });
+    }
+
+    //Delte Posts
+    static deletePost(id){
+        return axios.delete(`${url}${id}`)
+    }
+}
+
+export default PostService;
