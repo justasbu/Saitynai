cc3ef66ae65ea901b69a858a8bae58837bf28557
justas-GL62M-7RDX <justas.bucionis@gmail.com>
2020-09-22 23:42:36 +0300

    ready for heroku

---
+++ server/routes/api/posts.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get posts
+router.get('/', async (req,res) => {
+   const posts = await loadPostsCollection();
+   res.send(await posts.find({}).toArray());
+});
+//Add Post
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
+
+ 
+//Delete Post
+router.delete('/:_id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params._id)});
+    res.status(200).send('Deleted user /:_id');
+});
+
+//connection
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect(
+        'mongodb+srv://test:test@cluster0.7zz4o.gcp.mongodb.net/cluster0?retryWrites=true&w=majority',
+        {
+            useNewUrlParser: true
+        });
+
+        return client.db('cluster0').collection('posts');
+    
+}
+
+module.exports = router;
