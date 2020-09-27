adf97d131b6b548c90ed9c31208456709b1bed29
justas-GL62M-7RDX <justas.bucionis@gmail.com>
2020-09-27 20:38:34 +0300

    get 1

--- server/index.js
+++ server/index.js
@@ -11,6 +11,13 @@ app.use(cors());
 const posts = require('./routes/posts')
 app.use('/posts',posts)
 
+
+const providers = require('./routes/providers')
+app.use('/providers',posts)
+
+const clients = require('./routes/clients')
+app.use('/clients',clients)
+
 //Handle production
 
 if (process.env.NODE_ENV === 'production'){
