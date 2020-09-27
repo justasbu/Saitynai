7577db85698dd869ff88a26367dc8adf97fc20b7
justas-GL62M-7RDX <justas.bucionis@gmail.com>
2020-09-27 20:01:19 +0300

    get 1

--- server/index.js
+++ server/index.js
@@ -1,27 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser'); //parser for json
-const cors = require('cors');
-
+const path = require('path');
 const app = express(); 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-
+//Middleware
 app.use(bodyParser.json())
-app.use(cors());
+app.use(express.static(path.resolve('dist/')));
 
-const posts = require('./routes/posts')
-app.use('/posts',posts)
+//Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-//Handle production
+//route to index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('/dist/index.html'));
+});
 
-if (process.env.NODE_ENV === 'production'){
-    //static folder
-    app.use(express.static(__dirname + '/public'));
+//route to api/clients
+const clients = require('./routes/clients');
+app.use('/clients', clients);
 
-    //handle SPA
+//route to api/providers
+const providers = require('./routes/providers');
+app.use('/providers', providers);
 
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+// mongo and server
+const port = process.env.PORT_DOCKER;
+const options = {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex:true,
+};
 
-}
-const port = process.env.PORT || 5000;
+//start mongo
+mongoose.connect(`mongodb://${process.env.MONGO_HOST}/${process.env.MONGO_DB}`, options);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('WE ARE CONNECTED TO MONGODB!');
 
-app.listen(port, () => console.log(`Server started on port ${port}.`));
+    //and start server
+    app.listen(port, function () {
+        setTimeout(function() {
+            console.log(`LISTENING ON PORT ${process.env.PORT_MACHINE}...`);
+        }, 2000);
+
+    });
+});
