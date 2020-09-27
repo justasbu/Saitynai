93529be5ddfde164b73343915b7e226e16086c2b
justas-GL62M-7RDX <justas.bucionis@gmail.com>
2020-09-27 20:32:53 +0300

    get 1

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser'); //parser for json
-const path = require('path');
+const cors = require('cors');
+
 const app = express(); 
 
 
