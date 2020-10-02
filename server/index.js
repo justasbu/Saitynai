cbb1ba173fb9600efc934f7aac8aaaa2a6ce6774
justas-GL62M-7RDX <justas.bucionis@gmail.com>
2020-10-03 01:07:56 +0300

    get 1

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'production'){
 
     //handle SPA
 
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+    //app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 }
 const port = process.env.PORT || 5000;
