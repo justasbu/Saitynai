c5d5d4e511985032fba848a17a385fc246b6339f
justas-GL62M-7RDX <justas.bucionis@gmail.com>
2020-10-05 22:58:13 +0300

    again

--- server/index.js
+++ server/index.js
@@ -20,11 +20,11 @@ app.use('/api/orders', orders);
 if(process.env.NODE_ENV === 'production'){
 
     //Static folder
-    app.use(express.static(__dirname + '/client/public/'));
+    app.use(express.static(__dirname + './public/'));
 
     //Handle SPA
 
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + './public/index.html'));
 }
 
 const port = process.env.PORT || 5000;
