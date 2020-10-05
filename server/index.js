bbefe33e28f58d2cf9af079888609878faf3c5f3
justas-GL62M-7RDX <justas.bucionis@gmail.com>
2020-10-05 20:27:31 +0300

    again

--- server/index.js
+++ server/index.js
@@ -17,6 +17,16 @@ app.use('/api/clients', clients);
 const orders = require('./routes/api/orders')
 app.use('/api/orders', orders);
 
+if(process.env.NODE_ENV === 'production'){
+
+    //Static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    //Handle SPA
+
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}.`));
