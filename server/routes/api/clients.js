2f1940aebdc17c4ca4094db1ea8f80defb86462c
justas-GL62M-7RDX <justas.bucionis@gmail.com>
2020-10-05 21:43:52 +0300

    again

--- server/routes/api/clients.js
+++ server/routes/api/clients.js
@@ -30,6 +30,12 @@ router.delete('/:id', async (req,res) =>{
     await clients.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
     res.status(200).send();  // ??? clients.deleteOne({_id: req.params.id}
 })
+router.patch('/:id',async (req,res) => {
+    const clients = await loadClientsCollection();
+    var ObjectId = require('mongodb').ObjectID
+    var updateObject = req.body;
+    res.send(await clients.updateOne({_id : ObjectId(req.params.id)}, {$set: updateObject}));
+});
 
 async function loadClientsCollection(){
     const client = await mongodb.MongoClient.connect("mongodb+srv://test:test@cluster0.7zz4o.gcp.mongodb.net/cluster0?retryWrites=true&w=majority",{
--- server/routes/api/orders.js
+++ server/routes/api/orders.js
@@ -30,6 +30,12 @@ router.delete('/:id', async (req,res) =>{
     await orders.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
     res.status(200).send();  // ??? orders.deleteOne({_id: req.params.id}
 })
+router.patch('/:id',async (req,res) => {
+    const orders = await loadOrdersCollection();
+    var ObjectId = require('mongodb').ObjectID
+    var updateObject = req.body;
+    res.send(await orders.updateOne({_id : ObjectId(req.params.id)}, {$set: updateObject}));
+});
 
 async function loadOrdersCollection(){
     const client = await mongodb.MongoClient.connect("mongodb+srv://test:test@cluster0.7zz4o.gcp.mongodb.net/cluster0?retryWrites=true&w=majority",{
